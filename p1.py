aef04a8ef81f8696fc7bc110b32fdc99e799296b
rpmaes <roci_pmaes@hotmail.com>
2021-01-29 23:24:57 +0100

    Funciones clásicas

--- p1.py
+++ p1.py
@@ -1,8 +1,8 @@
 def sumaTodos(limitTo):
     resultado = 0
     for i in range (0, limitTo+1):
-        resultado += 1
+        resultado += i
         
     return resultado
 
-print(sumaTodos(100))
+print(sumaTodos(4))
